aa0dd2ca0fb49624493387023726a17ca152194e
Ojash Gurung <ojasggg@gmail.com>
2025-02-13 17:58:41 -0500

    Database Initialized

--- backend/app/database/db.py
+++ backend/app/database/db.py
@@ -1,20 +1,23 @@
 from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
 from sqlalchemy.orm import sessionmaker
-from sqlmodel import SQLModel, create_engine
+from sqlmodel import SQLModel
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 from ..config import Config
 
-async_engine = AsyncEngine(create_async_engine(url=Config.DATABASE_URL, echo=True))
+async_engine: AsyncEngine  = create_async_engine(url=Config.DATABASE_URL, echo=True)
+
+AsyncSessionLocal = sessionmaker(
+    bind=async_engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
 
 async def init_db() -> None:
     async with async_engine.begin() as conn:
         await conn.run_sync(SQLModel.metadata.create_all)
+    print("✅ Database connected successfully!")  # Log for DB connection
 
 async def get_session() -> AsyncSession:
-    async_session = sessionmaker(
-        bind=async_engine, class_=AsyncSession, expire_on_commit=False
-    )
-
-    async with async_session() as session:
+    async with AsyncSessionLocal() as session:
         yield session
