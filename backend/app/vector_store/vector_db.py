a9b2686c1a505b30dba7f9b499045493ef5674aa
Ojash Gurung <ojasggg@gmail.com>
2025-02-14 19:52:00 -0500

    Vector DB initialization is done

---
+++ backend/app/vector_store/vector_db.py
@@ -0,0 +1,30 @@
+from ..config import Config
+import asyncio
+
+from pinecone import PineconeAsyncio, ServerlessSpec
+
+index_name = "SelfAI"
+
+async def main():
+    async with PineconeAsyncio(api_key=Config.PINECONE_API_KEY) as pc:
+        if not await pc.has_index(index_name):
+            await pc.create_index(
+                name="SelfAI",
+                dimension=1536,
+                metric="cosine",
+                spec=ServerlessSpec(
+                    cloud="aws",
+                    region="us-east-1"
+                ),
+                deletion_protection="disabled",
+                tags={
+                    "environment": "development"
+                }
+            )
+            print(f"Index '{index_name}' created successfully.")
+        else:
+            print(f"Index '{index_name}' already exists.")
+            
+
+if __name__ == "__main__":
+    asyncio.run(main())
