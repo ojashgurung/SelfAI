8105ccd6068c547eb42571d38b4895121402f0b8
Ojash Gurung <ojasggg@gmail.com>
2025-03-05 14:57:14 -0500

    fix: Major Database table schema fixes

--- backend/app/database/models.py
+++ backend/app/database/models.py
@@ -1,8 +1,8 @@
 import uuid as uuid_pkg
 from datetime import datetime
-from typing import Optional
+from typing import Optional, List
 from enum import Enum
-
+from sqlalchemy import JSON
 from sqlmodel import Field, SQLModel, Relationship
 
 
@@ -12,6 +12,7 @@ class UserRole(str, Enum):
 
 class Users(SQLModel, table = True):
     uuid: uuid_pkg.UUID = Field(default_factory=uuid_pkg.uuid4, nullable=False, primary_key=True)
+    fullname: str = Field(nullable=False)
     email: str = Field(unique=True, nullable=False)
     password_hash: str = Field(nullable=False, exclude=True)
     created_at: datetime = Field(default_factory=datetime.now)
@@ -24,21 +25,22 @@ class Users(SQLModel, table = True):
     rag_enabled: bool = Field(default=True)
     is_premium: bool = Field(default=False)
     role: UserRole = Field(default= UserRole.USER)
-    data_sources: list["UserDataSource"] = Relationship(back_populates="Users")
+    Documents: list["Documents"] = Relationship(back_populates="Users")
 
     def __repr__(self):
         return f"<User {self.email} - Premium: {self.is_premium}>"
     
-class UserDataSource(SQLModel, table = True):
+class Documents(SQLModel, table = True):
     id: uuid_pkg.UUID = Field(default_factory=uuid_pkg.uuid4, nullable=False, primary_key=True)
-    uuid: uuid_pkg.UUID = Field(foreign_key="users.uuid", nullable=False)
-    source: str = Field(nullable=False) 
-    vector_db_id: str = Field(nullable=False)
-    data: Optional[str] = Field(default=None)
+    user_id: uuid_pkg.UUID = Field(foreign_key="users.uuid", nullable=False)
+    file_name: str = Field(nullable=False)
+    file_path: str
+    vector_ids: List[str] = Field(sa_type=JSON, default=[])
+    namespace: str
     created_at: datetime = Field(default_factory=datetime.now)
     update_at: datetime = Field(default_factory=datetime.now)
 
-    Users: "Users" = Relationship(back_populates="data_sources")
+    Users: "Users" = Relationship(back_populates="Documents")
 
     def __repr__(self):
-        return f"<UserDataSource(user_id={self.uuid}, source={self.source}, created_at={self.created_at})>"
+        return f"<Documents(user_id={self.user_id}, source={self.file_name}, created_at={self.created_at})>"
