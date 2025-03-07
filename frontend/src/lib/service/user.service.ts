f6abfd2bbc8a9055fab9cf3c260c3b9d951acc50
Ojash Gurung <ojasggg@gmail.com>
2025-03-07 18:35:07 -0500

    feat: User service function to handle user API calls for frontend currently done for get user

---
+++ frontend/src/lib/service/user.service.ts
@@ -0,0 +1,27 @@
+const API_BASE_URL = "http://localhost:8000/api/v1/user";
+
+export interface UserInfo {
+  email: string;
+  fullname: string;
+}
+
+export const UserService = {
+  async getCurrentUser(): Promise<UserInfo> {
+    const accessToken = document.cookie
+      .split("; ")
+      .find((row) => row.startsWith("access_token="))
+      ?.split("=")[1];
+
+    const response = await fetch(`${API_BASE_URL}/me`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch user info");
+    }
+
+    return response.json();
+  },
+};
