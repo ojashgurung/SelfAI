9277080c4d8e3da74fe67ad8e33e12d0009e5cde
Ojash Gurung <ojasggg@gmail.com>
2025-04-09 23:47:40 -0400

    feat: add EmbedShareConnect and TryItNow components to landing page
    This commit introduces the EmbedShareConnect and TryItNow components to the landing page, enhancing the user experience by providing additional interactive sections.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 import AnalyticsSection from "@/components/landing-page/analytics-section/analytics-section";
+import { EmbedShareConnect } from "@/components/landing-page/embed-share-connect/enbed-share-connect";
 import { Footer } from "@/components/landing-page/footer/footer";
 import { HeroSection } from "@/components/landing-page/hero/hero-section";
 import { HowItWorks } from "@/components/landing-page/how-it-works/how-it-works";
+import { TryItNow } from "@/components/landing-page/try-it-now/try-it-now";
 
 export default function Hero() {
   return (
@@ -10,6 +12,10 @@ export default function Hero() {
       <HeroSection />
       <HowItWorks />
       <AnalyticsSection />
+      <EmbedShareConnect />
+      <div className="relative -mb-32">
+        <TryItNow />
+      </div>
       <Footer />
     </>
   );
